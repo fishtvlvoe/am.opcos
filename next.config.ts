6764a003bb0b7f209a95629b47b21520993191ba
fish老魚 <fish@fishot.com>
2026-06-05 15:23:41 +0800

    fix(config): 在 images.remotePatterns 中加入 R2 domain 配置避免圖片載入崩潰

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "img.anismile.jp",
 			},
+			{
+				protocol: "https",
+				hostname: "*.r2.dev",
+			},
 		],
 	},
 	webpack: (config, { webpack, isServer }) => {
